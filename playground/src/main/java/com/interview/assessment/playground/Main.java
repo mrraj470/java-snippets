52ce22646029d5edbe490d66fe4d632ac900b3fd
Rajkumar <mrraj470@gmail.com>
2021-10-26 10:45:56 +0530

    Add main

--- playground/src/main/java/com/interview/assessment/playground/Main.java
+++ playground/src/main/java/com/interview/assessment/playground/Main.java
@@ -1,4 +1,16 @@
 package com.interview.assessment.playground;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Node head = new Node(0);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(4);
+
+        Printer printer = new Printer();
+        System.out.println("Out: " + printer.print(head));
+    }
+
 }
---
+++ playground/src/main/java/com/interview/assessment/playground/Printer.java
@@ -0,0 +1,8 @@
+package com.interview.assessment.playground;
+
+public class Printer {
+    public String print(Node head) {
+        //code here
+        return null;
+    }
+}
